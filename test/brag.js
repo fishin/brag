4cfec930ccab0eab7b2bc113c1c35c630cd7c923
lloydbenson <lloyd.benson@gmail.com>
2014-12-23 06:52:47 -0600

    start primitive brag module

---
+++ test/brag.js
@@ -0,0 +1,39 @@
+var Code = require('code');
+var Hapi = require('hapi');
+var Lab = require('lab');
+var Brag = require('../lib');
+
+var internals = {};
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
+
+var bragPath = '/tmp/testbrag';
+
+describe('brag', function () {
+
+    it('getNotifyCommand new', function (done) {
+        var notify = {
+            to: 'lloyd.benson@gmail.com',
+            subject: '[ficion] subject',
+            body: 'body'
+        };
+        var brag = new Brag;
+        var command = brag.getNotifyCommand(notify);
+        //console.log(command);
+        expect(command).to.contain('echo "body" | /usr/bin/mail -s "[ficion] subject" lloyd.benson@gmail.com');
+        done();
+    });
+
+    it('getElements', function (done) {
+
+        var brag = new Brag;
+        var elements = brag.getElements();
+        //console.log(elements);
+        expect(elements).to.be.length(3);
+        done();
+    });
+
+});
