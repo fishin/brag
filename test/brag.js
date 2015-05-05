fc3eef4e214b83c74c71327db81e34f324408c11
lloydbenson <lloyd.benson@gmail.com>
2015-05-05 09:37:47 -0500

    make it so you have to have a transport defined

--- test/brag.js
+++ test/brag.js
@@ -2,7 +2,17 @@ var Code = require('code');
 var Lab = require('lab');
 var Brag = require('../lib');
 
-var internals = {};
+var internals = {
+    defaults: {
+        transporter: {
+            service: 'gmail',
+            auth: {
+                user: 'lloyd.benson@gmail.com',
+                pass: 'password'
+            }
+        }
+    }
+};
 
 var lab = exports.lab = Lab.script();
 var expect = Code.expect;
@@ -15,22 +25,38 @@ describe('brag', function () {
 
         var notify = {
             to: 'lloyd.benson@gmail.com',
-            subject: '[ficion] subject',
+            subject: 'subject',
+            message: 'message'
+        };
+        var brag = new Brag(internals.defaults);
+        brag.notify(notify, function (err, info) {
+
+            //console.log(err);
+            //console.log(info);
+            expect(err.code).to.equal('EAUTH');
+            done();
+        });
+    });
+
+    it('notify no transporter', function (done) {
+
+        var notify = {
+            to: 'lloyd.benson@gmail.com',
+            subject: 'subject',
             message: 'message'
         };
         var brag = new Brag({});
         brag.notify(notify, function (err, info) {
 
             //console.log(err);
-            //console.log(info);
-            expect(err).to.exist();
+            expect(err).to.equal('no transporter provided');
             done();
         });
     });
 
     it('getElements', function (done) {
 
-        var brag = new Brag({});
+        var brag = new Brag(internals.defaults);
         var elements = brag.getElements();
         expect(elements).to.be.length(3);
         done();
